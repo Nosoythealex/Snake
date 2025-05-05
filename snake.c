238e0be1a951bcb0ef637002071a60def7670f2b
Alejandro Mercado <gostfull010@gmail.com>
2025-05-05 03:26:33 -0600

    Corregi definiciones para las nuevas funciones

--- snake.c
+++ snake.c
@@ -1,4 +1,4 @@
-/*
+lea/*
 ##############################################
 #                                            #
 #     Alberto Alejandro Mercado Gómez        #
@@ -14,11 +14,11 @@
 #include <stdio.h>
 
 // Definimos las medidas exactas de nuestro pantalla
-#define BOARD_WIDTH 34 // BOARD_WIDTH - 1 para el indice base 0
-#define BOARD_HEIGHT 24 // BOARD_HEIGHT -1
+#define WIDTH 34 // BOARD_WIDTH - 1 para el indice base 0
+#define HEIGHT 24 // BOARD_HEIGHT -1
 
-#define led_matrix_wid 35 // Ancho exacto establecido en Ripes
-#define led_matrix_hei 25 // Alto exacto establecido en Ripes
+#define matrix_wid 35 // Ancho exacto establecido en Ripes
+#define matrix_hei 25 // Alto exacto establecido en Ripes
 
 #define COLOR_SNAKE_HEAD 0x00FF00 // Color para la cabeza
 #define COLOR_SNAKE_BODY 0x00C000 // Color para el cuerpo
@@ -29,17 +29,17 @@ volatile unsigned int *switch0 = (volatile unsigned int *)SWITCHES_0_BASE;
 // LEDS
 volatile unsigned int *baseLed = (volatile unsigned int *)LED_MATRIX_0_BASE; // Cast a entero
 // D_Pads
-volatile unsigned int *d_padUp = (volatile unsigned int *)D_PAD_0_UP;
-volatile unsigned int *d_padDo = (volatile unsigned int *)D_PAD_0_DOWN;
-volatile unsigned int *d_padLe = (volatile unsigned int *)D_PAD_0_LEFT;
-volatile unsigned int *d_padRi = (volatile unsigned int *)D_PAD_0_RIGHT;
+volatile unsigned int *dpadUp = (volatile unsigned int *)D_PAD_0_UP;
+volatile unsigned int *dpadDo = (volatile unsigned int *)D_PAD_0_DOWN;
+volatile unsigned int *dpadLe = (volatile unsigned int *)D_PAD_0_LEFT;
+volatile unsigned int *dpadRi = (volatile unsigned int *)D_PAD_0_RIGHT;
 
 // Registro para el estado de los LED's
 unsigned int led_state[25][35];
 
 // Array para Posicion para el cuerpo y coordenadas
-int snake_bx[100];
-int snake_by[100];
+int snakex[100];
+int snakey[100];
 int snake_length =1;
 
 // Posicion de la manzana
@@ -52,8 +52,7 @@ volatile unsigned int *apple =0;
 
 
 // Variables globales
-unsigned int numRandom = 1;
-int in_game = 0;
+int ingame = 0;
 int direction = 1; // 1=UP, 2=DOWN(malito), 3=LEFT 4=RIGHT
 int score = 1;
 int head_x = 0;    
